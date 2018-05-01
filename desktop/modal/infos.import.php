cf2438c5bde4cf4c2db9a12d7ef6c1d6e0c8e5ba
KiboOst <KiboOst@users.noreply.github.com>
2018-05-01 10:40:25 +0200

    Champ rechercher

--- desktop/modal/infos.import.php
+++ desktop/modal/infos.import.php
@@ -19,28 +19,56 @@
 if (!isConnect('admin')) {
     throw new Exception('{{401 - Accès non autorisé}}');
 }
-  echo '<a class="btn btn-success pull-right" id="bt_import" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Importer}}</a>';
+    echo '<div class="col-sm-12">';
+        echo '<div class="col-sm-11">';
+            echo '<input class="form-control" placeholder="{{Rechercher}}" style="margin-bottom:4px;" id="in_searchCmds" onkeyup="filterCmd()"/>';
+        echo '</div>';
+    echo '<a class="btn btn-success pull-right" id="bt_import" style="margin-bottom:4px;"><i class="fa fa-plus-circle"></i> {{Importer}}</a>';
+    echo '</div>';
 
+  echo '<div id="cmdsDisplay">';
   $cmds = cmd::all();
   foreach($cmds as $cmd)
   {
     $type = $cmd->getType();
     if ($cmd->getType() == 'info')
     {
-    	$name = $cmd->getHumanName();
-      	
-      	$div = '<div class="log col-sm-12" style="padding-top:5px">';
-      	$div .= '<div class="form-group">';
-      	$div .= '<input type="checkbox" id="isEnable" class="expressionAttr col-sm-1" data-l1key="options" style="width:20px" title="{{Cocher pour importer la commande}}" />';
-      	$div .= '<div class="col-sm-10">';
-		$div .= '<input type="text" class="form-control" id="cmdName"  value="'.$name.'" readonly />';
-      	$div .= '</div>';
-      	$div .= '</div>';
-      	$div .= '</div>';
-      echo $div;
-  	}
+        $name = '#'.$cmd->getHumanName().'#';
+        
+        $div = '<div class="log col-sm-12" style="display:;padding-top:5px">';
+        $div .= '<div class="form-group">';
+        $div .= '<input type="checkbox" id="isEnable" class="expressionAttr col-sm-1" data-l1key="options" style="width:20px" title="{{Cocher pour importer la commande}}" />';
+        $div .= '<div class="col-sm-10">';
+        $div .= '<input type="text" class="form-control" id="cmdName"  value="'.$name.'" readonly />';
+        $div .= '</div>';
+        $div .= '</div>';
+        $div .= '</div>';
+        echo $div;
+    }
   }
+  echo '</div>';
 
 ?>
+
+<script>
+function filterCmd() {
+    input = document.getElementById('in_searchCmds');
+    filter = input.value.toUpperCase();
   
+    $('#cmdsDisplay .log').each(function ()
+    {
+        data = $(this).find("#cmdName").val()
+        if (data.toUpperCase().indexOf(filter) > -1)
+        {
+            $(this)[0].style.display = "";
+        } else {
+            $(this)[0].style.display = "none";
+        }
+
+    });
+    
+}
+</script>
+  
+
 <?php include_file('desktop', 'jeelog', 'js', 'jeelog');?>
