3d31962ed62763a2aad0c93802e30ae35a46bb26
KiboOst <KiboOst@users.noreply.github.com>
2019-03-19 19:41:10 +0100

    coreV4

--- desktop/php/jeelog.php
+++ desktop/php/jeelog.php
@@ -9,60 +9,51 @@ $eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
 <div class="row row-overflow">
-    <div class="col-lg-2 col-md-3 col-sm-4">
-        <div class="bs-sidebar">
-            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un jeelog}}</a>
-                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-                <?php
-foreach ($eqLogics as $eqLogic) {
-  $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-  echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity .'"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
-}
-        ?>
-           </ul>
-       </div>
-   </div>
-
-   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-  <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
-  <div class="eqLogicThumbnailContainer">
-      <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-        <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
+  <div class="col-xs-12 eqLogicThumbnailDisplay" style="padding-left: 25px;">
+    <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+    <div class="eqLogicThumbnailContainer">
+        <div class="cursor eqLogicAction success" data-action="add">
+          <i class="fas fa-plus-circle"></i>
+          <br>
+          <span>{{Ajouter}}</span>
+      </div>
+      <div class="cursor eqLogicAction" data-action="gotoPluginConf">
+        <i class="fas fa-wrench"></i>
         <br>
-        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
+        <span>{{Configuration}}</span>
+      </div>
     </div>
-      <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-      <i class="fa fa-wrench" style="font-size : 6em;color:#767676;"></i>
-    <br>
-    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
+    <legend><i class="fa fa-table"></i> {{Mes jeelogs}}</legend>
+    <div class="eqLogicThumbnailContainer">
+      <?php
+        foreach ($eqLogics as $eqLogic) {
+          $opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+          echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+          echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+          echo '<br>';
+          echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+          echo '</div>';
+        }
+      ?>
+    </div>
   </div>
-  </div>
-  <legend><i class="fa fa-table"></i> {{Mes jeelogs}}</legend>
-<div class="eqLogicThumbnailContainer">
-    <?php
-foreach ($eqLogics as $eqLogic) {
-  $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-  echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-  echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
-  echo "<br>";
-  echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
-  echo '</div>';
-}
-?>
-</div>
-</div>
 
-<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-  <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-  <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-  <a class="btn btn-default eqLogicAction pull-right" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
-  <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
+<div class="col-xs-12 eqLogic" style="padding-left: 25px;display: none;">
+  <div class="input-group pull-right" style="display:inline-flex">
+    <span class="input-group-btn">
+      <a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}
+      </a><a class="btn btn-sm btn-default eqLogicAction" data-action="copy"><i class="fa fa-files-o"></i> {{Dupliquer}}
+      </a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}
+      </a><a class="btn btn-sm btn-danger eqLogicAction roundedRight" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+    </span>
+  </div>
+
   <ul class="nav nav-tabs" role="tablist">
     <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
     <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
     <li role="presentation"><a href="#logtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Logs}}</a></li>
   </ul>
+
   <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
 
   <div role="tabpanel" class="tab-pane active" id="eqlogictab">
@@ -109,7 +100,6 @@ foreach ($eqLogics as $eqLogic) {
             <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
           </div>
         </div>
-
 
         <hr>
         <div class="form-group expertModeVisible">
@@ -158,12 +148,12 @@ foreach ($eqLogics as $eqLogic) {
     </form>
   </div>
 
-
     <div role="tabpanel" class="tab-pane" id="logtab">
-      <a class="btn btn-default pull-left" id="bt_importinfos" style="margin-top: 5px;" ><i class="fa fa-cog"></i> {{Import infos}}</a>
-      <a class="btn btn-success pull-right" id="bt_addLogfile" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter Log}}</a>
-      <a class="btn btn-success pull-right" id="bt_addScenario" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter Scenario}}</a>
-      <a class="btn btn-success pull-right" id="bt_addCmd" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter Commande}}</a><br/><br/>
+      <a class="btn btn-success pull-left" id="bt_importinfos" style="margin-top: 5px;" ><i class="fa fa-cog"></i> {{Import infos}}
+      </a><a class="btn btn-success pull-right" id="bt_addLogfile" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter Log}}
+      </a><a class="btn btn-success pull-right" id="bt_addScenario" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter Scenario}}
+      </a><a class="btn btn-success pull-right" id="bt_addCmd" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter Commande}}</a>
+      <br/><br/>
 
       <div id="div_logs"></div>
     </div>
